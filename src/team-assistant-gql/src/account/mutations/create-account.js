7107564228a6889b88d73f7fd55be4dc1889154a
Trond Bergquist <trond_bergquist@hotmail.com>
2020-10-11 22:31:55 +0200

    tag: Add create account tests

--- src/team-assistant-gql/src/account/mutations/create-account.js
+++ src/team-assistant-gql/src/account/mutations/create-account.js
@@ -11,6 +11,9 @@ type Args = {
   ...
 };
 
+// eslint-disable-next-line no-control-regex
+const emailRexEx = /(?:[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+
 export default {
   type: CreateAccountResponse,
   args: {
@@ -22,6 +25,18 @@ export default {
     },
   },
   resolve: async (_: mixed, { email, password }: Args): mixed => {
+    if (!emailRexEx.test(email)) {
+      const error = {};
+      error.reason = 'INVALID_EMAIL';
+      error.message = `${email} is not a valid email address`;
+      return error;
+    }
+    if (password === '') {
+      const error = {};
+      error.reason = 'MISSING_PASSWORD';
+      error.message = 'Password is mandatory';
+      return error;
+    }
     try {
       const user = await UserModel.createUser({ email, password });
       return user;
--- src/team-assistant-gql/src/index.js
+++ src/team-assistant-gql/src/index.js
@@ -1,14 +1,8 @@
 /* eslint-disable no-console */
 // @flow
 
-import express from 'express';
-import { ApolloServer } from 'apollo-server-express';
-import cors from 'cors';
-import compression from 'compression';
-import morgan from 'morgan';
-
-import schema from './schema';
 import connection from './database/connection';
+import app from './app';
 
 const uri = process.env.MONGO_DB_URL ?? 'mongodb://127.0.0.1:27017/team_assistant';
 connection.openUri(uri, {
@@ -16,15 +10,6 @@ connection.openUri(uri, {
   useNewUrlParser: true,
 });
 
-const server = new ApolloServer({ schema });
-
-const app = express();
-app.use(cors({ methods: ['GET', 'POST'] }));
-app.use(compression());
-app.use(morgan('dev'));
-
-server.applyMiddleware({ app });
-
-app.listen({ port: 4000 }, () =>
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`),
+app.listen({ port: process.env.PORT ?? 4000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000/graphql`),
 );
