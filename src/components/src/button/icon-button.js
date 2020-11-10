61c21a644649f2e444dc1f076a6ebd01763618e3
Trond Bergquist <trond_bergquist@hotmail.com>
2020-11-10 11:40:32 +0100

    components: Add icon button

---
+++ src/components/src/button/icon-button.js
@@ -0,0 +1,55 @@
+// @flow
+
+import { type Node, type Element, cloneElement } from 'react';
+import { create } from '@adeira/sx';
+
+// TODO: Allow more types and sizes
+type Props = {
+  +'children': Element<any>,
+  +'onClick'?: () => void,
+  +'aria-label': string,
+};
+
+export default function IconButton({ children, ...rest }: Props): Node {
+  return (
+    <button {...rest} type="button" className={styles('button', 'primary', 'normal')}>
+      {cloneElement(children, { className: styles('icon') })}
+    </button>
+  );
+}
+
+const styles = create({
+  button: {
+    'position': 'relative',
+    'color': 'var(--color-white)',
+    'border': 'none',
+    'borderRadius': '50%',
+    'outline': 'none',
+    'fontFamily': 'inherit',
+    'transition': 'transform .3s',
+    'cursor': 'pointer',
+    ':hover': {
+      transform: 'translateY(-3px)',
+    },
+    ':active': {
+      transform: 'translateY(-1px)',
+    },
+  },
+  primary: {
+    'backgroundColor': 'var(--color-primary)',
+    ':focus': {
+      boxShadow: 'var(--color-primary-focus) 0px 0px 0px 0.2rem',
+    },
+  },
+  normal: {
+    fontSize: '1.6rem',
+    height: '4rem',
+    width: '4rem',
+  },
+  icon: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+  },
+});
