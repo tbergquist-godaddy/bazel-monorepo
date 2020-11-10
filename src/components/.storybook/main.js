38848d35c76b55d272f2acfa41a12bba4fa10fb2
Trond Bergquist <trond_bergquist@hotmail.com>
2020-11-10 13:43:42 +0100

    components: Add storybook viewport and a11y addons

--- src/components/.storybook/main.js
+++ src/components/.storybook/main.js
@@ -2,17 +2,44 @@
 
 module.exports = {
   stories: ['../src/**/*.stories.[tj]s'],
-  addons: ['@storybook/addon-actions'],
+  addons: [
+    '@storybook/addon-actions/register',
+    '@storybook/addon-viewport/register',
+    '@storybook/addon-a11y',
+  ],
   webpackFinal: (config /* :Object */) /* :Object */ => {
-    config.module.rules.push({
-      test: (/\.(?:js)$/ /*: RegExp */),
-      use: {
-        loader: 'babel-loader',
-        options: {
-          sourceType: 'unambiguous',
+    config.watchOptions = { poll: 1000 };
+
+    // remove svg from existing rule
+    config.module.rules = config.module.rules.map((rule) => {
+      if (
+        String(rule.test) ===
+        String(/\.(svg|ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani|pdf)(\?.*)?$/)
+      ) {
+        return {
+          ...rule,
+          test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani|pdf)(\?.*)?$/,
+        };
+      }
+
+      return rule;
+    });
+
+    config.module.rules.push(
+      {
+        test: (/\.(?:js)$/ /*: RegExp */),
+        use: {
+          loader: 'babel-loader',
+          options: {
+            sourceType: 'unambiguous',
+          },
         },
       },
-    });
+      {
+        test: (/\.svg$/ /*: RegExp  */),
+        use: ['@svgr/webpack'],
+      },
+    );
 
     return config;
   },
