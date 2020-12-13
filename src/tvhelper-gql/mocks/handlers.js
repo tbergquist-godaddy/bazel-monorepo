f3e5d804c569db271ad292be5b713b00443c1c16
Trond Bergquist <trond_bergquist@hotmail.com>
2020-12-13 13:08:12 +0100

    use @tbergq/mock-server in tvhelper-gql

--- src/tvhelper-gql/mocks/handlers.js
+++ src/tvhelper-gql/mocks/handlers.js
@@ -1,17 +1,20 @@
 // @flow
 
-import { rest } from 'msw';
+import { Handler } from '@tbergq/mock-server';
 
 import responses from './responses';
 
-const handlers: $FlowFixMe = [];
+const handlers: Handler[] = [];
 
 for (const key of Object.keys(responses)) {
-  handlers.push(
-    rest.get(key, (req, res, ctx) => {
+  const handler = new Handler({
+    url: key,
+    type: 'get',
+    handler: (req, res, ctx) => {
       return res(ctx.status(200), ctx.json(responses[key]));
-    }),
-  );
+    },
+  });
+  handlers.push(handler);
 }
 
 export default handlers;
