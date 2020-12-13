bd9f8aad927a6033d7f22ef64503721796e1dc18
Trond Bergquist <trond_bergquist@hotmail.com>
2020-12-13 15:28:41 +0100

    tj: add is logged in hook

--- src/trainingjournal/src/home/home.js
+++ src/trainingjournal/src/home/home.js
@@ -3,8 +3,11 @@
 import type { ComponentType } from 'react';
 import { Link } from '@tbergq/router';
 
+import useIsLoggedIn from '../services/use-is-logged-in';
+
 type Props = {};
 
 export default (function Home() {
+  useIsLoggedIn();
   return <Link to="/programs">programs</Link>;
 }: ComponentType<Props>);
