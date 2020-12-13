95793b9ee0484552fee3b7b9427864ae6132235f
Trond Bergquist <trond_bergquist@hotmail.com>
2020-12-13 11:59:36 +0100

    move common file mocks to jest workspace

--- src/components/jest.config.js
+++ src/components/jest.config.js
@@ -1,12 +1,6 @@
 // @flow
 
-const path = require('path');
-
 module.exports = {
   rootDir: __dirname,
   preset: '@tbergq/jest-preset',
-  moduleNameMapper: {
-    '\\.(svg)$': (path.join(__dirname, 'mocks', 'svgFileMock.js') /*: string  */),
-    '\\.(css)$': (path.join(__dirname, 'mocks', 'css-file-mock.js') /*: string  */),
-  },
 };
