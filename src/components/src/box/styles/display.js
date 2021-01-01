d352bf87dc6222f43d35057d0602c60d1649dedb
Trond Bergquist <trond_bergquist@hotmail.com>
2021-01-01 15:10:18 +0100

    components: remove sx from box

--- src/components/src/box/styles/display.js
+++ src/components/src/box/styles/display.js
@@ -1,49 +1,27 @@
 // @flow
 
-/* eslint-disable sx/valid-usage */
-import { create } from '@adeira/sx';
-
-import breakpoints, { breakpointsMap } from '../../breakpoints';
 import type { MediaObjectOr } from './types';
+import '../../utilities/display.css';
 
 type DisplayEnum = 'block' | 'inline' | 'inline-block' | 'flex';
 export type Display = MediaObjectOr<DisplayEnum>;
-
-const displayValues: $ReadOnlyArray<DisplayEnum> = ['block', 'inline', 'inline-block', 'flex'];
-const styleMap = {};
-
-for (const bp of breakpointsMap) {
-  for (const value of displayValues) {
-    if (bp === '') {
-      styleMap[`${bp}${value}`] = { display: value };
-    } else {
-      styleMap[`${bp}${value}`] = {
-        [breakpoints[bp]]: { display: value },
-      };
-    }
-  }
-}
-
-const styles = create(styleMap);
 
 export default function getDisplayStyles(display: ?Display): ?string {
   if (display == null) {
     return null;
   }
   if (typeof display === 'string') {
-    // $FlowExpectedError[incompatible-call]
-    return styles(display);
+    return `u-display-${display}`;
   }
   const classes = [];
 
   for (const key of Object.keys(display)) {
     if (key === '_') {
-      classes.push(display._);
+      classes.push(`u-display-${display._}`);
     } else {
-      const value = `${key}${display[key]}`;
-      classes.push(value);
+      classes.push(`u-display-${display[key]}-${key}`);
     }
   }
 
-  return styles(...classes);
+  return classes.join(' ');
 }
