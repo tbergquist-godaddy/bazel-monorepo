3391eddb9b78231986a7158d5ec680970f95a89c
Trond Bergquist <trond_bergquist@hotmail.com>
2021-01-01 16:03:36 +0100

    components: remove sx from button

--- src/components/src/button/icon-button.js
+++ src/components/src/button/icon-button.js
@@ -1,7 +1,7 @@
 // @flow
 
 import { type Node, type Element, cloneElement, type ComponentType } from 'react';
-import { create } from '@adeira/sx';
+import './button.css';
 
 // TODO: Allow more types and sizes
 type Props = {
@@ -26,61 +26,9 @@ export default function IconButton({
     <Component
       {...rest}
       type={as === 'button' ? 'button' : null}
-      className={styles('button', variant, size)}
+      className={`btn icon-btn btn--${variant} icon-btn--${size}`}
     >
-      {cloneElement(children, { className: styles('icon') })}
+      {cloneElement(children, { className: 'icon-btn__icon-container' })}
     </Component>
   );
 }
-
-const styles = create({
-  button: {
-    'position': 'relative',
-    'color': 'var(--color-white)',
-    'border': 'none',
-    'borderRadius': '50%',
-    'outline': 'none',
-    'fontFamily': 'inherit',
-    'transition': 'transform var(--transition-duration-normal)',
-    'cursor': 'pointer',
-    ':hover': {
-      transform: 'translateY(-3px)',
-    },
-    ':active:hover': {
-      transform: 'translateY(-1px)',
-    },
-  },
-  primary: {
-    'backgroundColor': 'var(--color-primary)',
-    ':focus': {
-      boxShadow: 'var(--color-primary-focus) 0px 0px 0px 0.2rem',
-    },
-  },
-  danger: {
-    'backgroundColor': 'var(--color-error)',
-    ':focus': {
-      boxShadow: 'var(--color-error-focus) 0px 0px 0px 0.2rem',
-    },
-  },
-  small: {
-    fontSize: '1.5rem',
-    height: '3rem',
-    width: '3rem',
-  },
-  normal: {
-    fontSize: '2rem',
-    height: '4rem',
-    width: '4rem',
-  },
-  large: {
-    fontSize: '3rem',
-    height: '6rem',
-    width: '6rem',
-  },
-  icon: {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-  },
-});
