d663fa6476090bc450c00115bb9c8ae2d9ea7bb1
Trond Bergquist <trond_bergquist@hotmail.com>
2021-01-01 16:45:22 +0100

    components: remove sx from modal

--- src/components/src/modal/modal.js
+++ src/components/src/modal/modal.js
@@ -1,11 +1,10 @@
 // @flow
 
 import { type Node, useEffect, useRef } from 'react';
-import { create } from '@adeira/sx';
 import { CSSTransition } from 'react-transition-group';
 
-import breakpoints from '../breakpoints';
 import './modal.css';
+import '../utilities/flex.css';
 
 type Props = {
   +isVisible: boolean,
@@ -49,83 +48,26 @@ export default function Modal({
         <div
           ref={backdropRef}
           onClick={handleClickOutside}
-          className={`modal__modal-backdrop ${styles('backdrop')}`}
+          className={`modal__modal-backdrop`}
           data-testid="modalBackdrop"
           role="dialog"
         >
-          <div className={`modal__modal-content ${styles('modal')}`}>
-            <div className={styles('header')}>
+          <div className={`modal__modal-content`}>
+            <div className="u-display-flex u-space-between u-align-center">
               <div>{title != null ? title : null}</div>
               <button
                 onClick={onClose}
                 aria-label={closeLabel}
-                className={styles('closeButton')}
+                className={'modal__close-button'}
                 type="button"
               >
                 &times;
               </button>
             </div>
-            <div className={styles('content')}>{children}</div>
+            <div className="u-margin-top-normal">{children}</div>
           </div>
         </div>
       </div>
     </CSSTransition>
   );
 }
-
-const styles = create({
-  backdrop: {
-    backgroundColor: 'rgba(0,0,0,0.5)',
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    // $FlowExpectedError[incompatible-call]
-    zIndex: 'var(--z-index-modal)',
-    overflow: 'auto',
-  },
-  modal: {
-    backgroundColor: 'var(--color-white)',
-    marginTop: '6rem',
-    marginBottom: '6rem',
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    padding: 'var(--space-normal)',
-    borderRadius: 'var(--border-radius-large)',
-    width: '100%',
-    [breakpoints.tablet]: {
-      maxWidth: 'min(750px, 80%)',
-    },
-    [breakpoints.desktop]: {
-      maxWidth: 'min(970px, 80%)',
-    },
-    [breakpoints.largeDesktop]: {
-      maxWidth: 'min(1170px, 80%)',
-    },
-  },
-  header: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  closeButton: {
-    'background': 'none',
-    'border': 'none',
-    'fontSize': '3rem',
-    'outline': 'none',
-    'padding': '0 var(--space-small)',
-    'cursor': 'pointer',
-    ':focus': {
-      backgroundColor: 'rgba(0,0,0, 0.1)',
-      borderRadius: '50%',
-    },
-    ':hover': {
-      backgroundColor: 'rgba(0,0,0, 0.1)',
-      borderRadius: '50%',
-    },
-  },
-  content: {
-    marginTop: 'var(--space-normal)',
-  },
-});
