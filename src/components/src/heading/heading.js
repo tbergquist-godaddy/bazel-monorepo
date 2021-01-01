44acb94411c5215bcac88fca9628321706e4ecd6
Trond Bergquist <trond_bergquist@hotmail.com>
2021-01-01 16:16:56 +0100

    components: remove sx from heading

--- src/components/src/heading/heading.js
+++ src/components/src/heading/heading.js
@@ -1,7 +1,10 @@
 // @flow
 
 import type { Node } from 'react';
-import { create } from '@adeira/sx';
+import classNames from 'classnames';
+
+import './heading.css';
+import '../utilities/text-align.css';
 
 type Levels = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 type Props = {
@@ -13,39 +16,13 @@ type Props = {
 
 export default function Heading({ level, children, align, as = level }: Props): Node {
   const Component = level;
-  return <Component className={styles('heading', as, align)}>{children}</Component>;
+  return (
+    <Component
+      className={classNames(`heading heading--${as}`, {
+        'u-text-align-center': align === 'center',
+      })}
+    >
+      {children}
+    </Component>
+  );
 }
-
-const styles = create({
-  heading: {
-    color: 'var(--color-text-black)',
-    fontWeight: '500',
-  },
-  center: {
-    textAlign: 'center',
-  },
-  h1: {
-    fontSize: '4rem',
-    margin: 'var(--space-x-large) 0',
-  },
-  h2: {
-    fontSize: '3.5rem',
-    margin: 'var(--space-x-large) 0',
-  },
-  h3: {
-    fontSize: '3rem',
-    margin: 'var(--space-large) 0',
-  },
-  h4: {
-    fontSize: '2.5rem',
-    margin: 'var(--space-normal) 0',
-  },
-  h5: {
-    fontSize: '2rem',
-    margin: 'var(--space-small) 0',
-  },
-  h6: {
-    fontSize: '1.6rem',
-    margin: 'var(--space-x-small) 0',
-  },
-});
