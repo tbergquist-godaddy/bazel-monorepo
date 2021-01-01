ee98377310411f3956ac5ed493ce729175ae7ff0
Trond Bergquist <trond_bergquist@hotmail.com>
2021-01-01 16:03:36 +0100

    components: remove sx from button

--- src/components/src/button/button.js
+++ src/components/src/button/button.js
@@ -1,10 +1,10 @@
 // @flow
 
 import type { Node, ComponentType } from 'react';
-import { create } from '@adeira/sx';
+import classNames from 'classnames';
 
 import Spinner from '../spinner/spinner';
-import breakpoints from '../breakpoints';
+import './button.css';
 
 type Props = {
   +'children': Node,
@@ -33,79 +33,13 @@ export default function Button({
     <Component
       disabled={isLoading}
       type={type}
-      className={styles(
-        'button',
-        variant,
-        size,
-        fullWidth === 'all' && 'fullWidth',
-        fullWidth === 'mediumMobile' && 'fullWidthUntilMediumMobile',
-      )}
+      className={classNames(`btn btn--${size} btn--${variant}`, {
+        'btn--full-width': fullWidth === 'all',
+        'btn--full-with-until-medium-mobile': fullWidth === 'mediumMobile',
+      })}
       {...rest}
     >
       {isLoading === true ? <Spinner size="small" color="white" /> : children}
     </Component>
   );
 }
-
-const styles = create({
-  button: {
-    'color': 'var(--color-white)',
-    'border': 'none',
-    'borderRadius': 'var(--border-radius-normal)',
-    'outline': 'none',
-    'fontWeight': 500,
-    'fontFamily': 'inherit',
-    'transition': 'transform var(--transition-duration-normal)',
-    'cursor': 'pointer',
-    'fontSize': 'inherit',
-    ':hover': {
-      transform: 'translateY(-3px)',
-    },
-    ':active:hover': {
-      transform: 'translateY(-1px)',
-    },
-  },
-  normal: {
-    padding: 'var(--space-normal) var(--space-large)',
-  },
-  small: {
-    padding: 'var(--space-small) var(--space-large)',
-    fontSize: '1.4rem',
-  },
-  primary: {
-    'backgroundColor': 'var(--color-primary)',
-    ':focus': {
-      boxShadow: 'var(--color-primary-focus) 0px 0px 0px 0.2rem',
-    },
-  },
-  secondary: {
-    'backgroundColor': 'var(--color-secondary)',
-    ':focus': {
-      boxShadow: 'var(--color-secondary-focus) 0px 0px 0px 0.2rem',
-    },
-  },
-  danger: {
-    'backgroundColor': 'var(--color-error)',
-    ':focus': {
-      boxShadow: 'var(--color-error-focus) 0px 0px 0px 0.2rem',
-    },
-  },
-  white: {
-    'backgroundColor': 'var(--color-white)',
-    'color': 'var(--color-black)',
-    'border': '1px solid var(--color-gray-light)',
-    ':focus': {
-      boxShadow: 'var(--color-gray-light-focus) 0px 0px 0px 0.2rem',
-      border: 'none',
-    },
-  },
-  fullWidth: {
-    width: '100%',
-  },
-  fullWidthUntilMediumMobile: {
-    width: '100%',
-    [breakpoints.mediumMobile]: {
-      width: 'unset',
-    },
-  },
-});
