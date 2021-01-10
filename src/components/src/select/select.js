7d9e9bea6d65b980b291cd6d0d9f326c291c65e5
Trond Bergquist <trond_bergquist@hotmail.com>
2021-01-10 14:08:03 +0100

    components: fix selext chevron position

--- src/components/src/select/select.js
+++ src/components/src/select/select.js
@@ -20,28 +20,30 @@ type Props = {
 
 function Select({ placeholder, options, error, label, ...rest }, ref): Node {
   return (
-    <label className={styles.Select__label}>
+    <label>
       <div className={styles['Select__label-container']}>{label}</div>
-      <select
-        {...rest}
-        className={classNames(styles.Select__select, {
-          [styles['Select__select--error']]: error != null,
-        })}
-        ref={ref}
-      >
-        {placeholder != null && <option value="">{placeholder}</option>}
-        {options.map((inputOption) => (
-          <option key={inputOption.value} value={inputOption.value}>
-            {inputOption.label}
-          </option>
-        ))}
-      </select>
+      <div className={styles['Select__select-container']}>
+        <select
+          {...rest}
+          className={classNames(styles.Select__select, {
+            [styles['Select__select--error']]: error != null,
+          })}
+          ref={ref}
+        >
+          {placeholder != null && <option value="">{placeholder}</option>}
+          {options.map((inputOption) => (
+            <option key={inputOption.value} value={inputOption.value}>
+              {inputOption.label}
+            </option>
+          ))}
+        </select>
+        <MdChevronRight className={styles.Select__arrow} />
+      </div>
       {error != null && (
         <div aria-live="polite" className={styles['Select__error-text']}>
           {error}
         </div>
       )}
-      <MdChevronRight className={styles.Select__arrow} />
     </label>
   );
 }
