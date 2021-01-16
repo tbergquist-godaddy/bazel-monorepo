b48d911b4c3231160c10bf615d7f3ce9002fac6b
Trond Bergquist <trond_bergquist@hotmail.com>
2021-01-16 16:10:41 +0100

    tj: fix bug where login after logout would show error state

--- src/trainingjournal/src/services/use-is-logged-in.js
+++ src/trainingjournal/src/services/use-is-logged-in.js
@@ -1,18 +1,17 @@
 // @flow
 
-import { useNavigate } from '@tbergq/router';
-import { useQueryClient } from 'react-query';
+import { useEffect } from 'react';
 
 import { TOKEN_KEY } from '../constants';
 
 export default function useIsLoggedIn() {
   const token = localStorage.getItem(TOKEN_KEY);
-  const navigate = useNavigate();
-  const cache = useQueryClient();
 
-  if (token == null) {
-    navigate('/');
-    cache.clear();
-    localStorage.removeItem(TOKEN_KEY);
-  }
+  useEffect(() => {
+    if (token == null) {
+      localStorage.removeItem(TOKEN_KEY);
+      // We need a hard reset, since cache.clear() triggers refetch of queries, https://github.com/tannerlinsley/react-query/discussions/431
+      window.location.href = '/';
+    }
+  }, [token]);
 }
