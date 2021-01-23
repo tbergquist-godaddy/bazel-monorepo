8f914800e6d0aa2ff4a99f0e627ad60d415f3b67
Trond Bergquist <trond_bergquist@hotmail.com>
2021-01-23 15:11:13 +0100

    tj-gql: remove test query

--- src/trainingjournal-gql/src/application/queries/index.js
+++ src/trainingjournal-gql/src/application/queries/index.js
@@ -1,37 +1,13 @@
 // @flow
 
-import { GraphQLObjectType, GraphQLString, GraphQLNonNull } from 'graphql';
-import globalId from '@adeira/graphql-global-id';
+import { GraphQLObjectType } from 'graphql';
 
 import me from './me';
-
-const Test = new GraphQLObjectType({
-  name: 'Test',
-  description: 'Just for test purpose, will be removed later',
-  fields: {
-    id: globalId(({ id }) => id),
-    firstName: { type: GraphQLString },
-    lastName: { type: GraphQLString },
-    nickname: { type: GraphQLString },
-  },
-});
 
 const RootQuery: GraphQLObjectType = new GraphQLObjectType({
   name: 'RootQuery',
   description: 'Root Query',
   fields: {
-    test: {
-      args: {
-        id: { type: GraphQLNonNull(GraphQLString) },
-      },
-      type: Test,
-      description: 'Just for testing purpose',
-      resolve: (_: mixed, { id }: { id: string, ... }) => ({
-        id,
-        firstName: 'Tito',
-        lastName: 'Bonito',
-      }),
-    },
     me,
   },
 });
