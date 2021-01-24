cfd6bd4f867829301eb92acbb09994c4a355610e
Trond Bergquist <trond_bergquist@hotmail.com>
2021-01-24 20:55:26 +0100

    tj-gql: add sets to day model

--- src/trainingjournal-gql/src/infrastructure/models/day.js
+++ src/trainingjournal-gql/src/infrastructure/models/day.js
@@ -3,17 +3,20 @@
 import { Schema, Model, type MongoId } from 'mongoose';
 
 import connections from '../connection';
+import SetModel, { SetSchema } from './set';
 
 export const DaySchema: Mongoose$Schema<DayModel> = new Schema({
   name: {
     type: String,
     required: true,
   },
+  sets: [SetSchema],
 });
 
 class DayModel extends Model {
   _id: MongoId;
   name: string;
+  sets: $ReadOnlyArray<SetModel>;
 }
 
 DaySchema.loadClass(DayModel);
